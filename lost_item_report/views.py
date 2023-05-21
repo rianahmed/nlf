fcb9a8acd70af38ef949f1c95d085bd6287dd733
mohiuddin06617 <mohiuddin06617@gmail.com>
2023-05-21 22:35:12 +0600

    Status field added userclaimitem model. Update status custom html page design and implemented

--- lost_item_report/views.py
+++ lost_item_report/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 
-from lost_item_report.models import FoundItem
+from lost_item_report.models import FoundItem, UserClaimItem, ClaimStatus
 
 
 def found_item_claim_view(request, id):
@@ -15,3 +15,16 @@ def found_item_claim_view(request, id):
         "back_url": reverse("admin:lost_item_report_founditem_changelist")
     }
     return render(request, 'admin/my_model_detail.html', context)
+
+
+def update_claim_item_status_view(request, id):
+    print("I am in update_claim_item_status_view")
+    all_claim_status = ClaimStatus.choices
+    claim_item_obj = UserClaimItem.objects.get(pk=id)
+    context = {
+        "all_claim_status": all_claim_status,
+        "claim_item_obj": claim_item_obj,
+        "submit_url": reverse("admin:lost_item_report_userclaimitem_add"),
+        "back_url": reverse("admin:lost_item_report_founditem_changelist")
+    }
+    return render(request, 'admin/claim_item_details.html', context)
