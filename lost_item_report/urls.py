0b53eda5baffe3f322bb259a78ee44188ca9b095
rianahmed <rianahmed83@gmail.com>
2023-06-10 20:11:41 +0600

    update-founditem-approve-status url added in urls.py

--- lost_item_report/urls.py
+++ lost_item_report/urls.py
@@ -4,10 +4,12 @@ from lost_item_report.views import (
     found_item_claim_view,
     update_claim_item_status_view,
     approve_found_item_view,
+    update_found_item_approve_item_view,
 )
 
 urlpatterns = [
     path('found-item-claim/<int:id>', found_item_claim_view, name='found-item-claim-url'),
     path('approve-found-item/<int:id>', approve_found_item_view, name='approve-found-item-url'),
     path('claim-item-status/<int:id>', update_claim_item_status_view, name='claim-item-status-url'),
+    path('update-founditem-approve-status/<int:id>', update_found_item_approve_item_view, name='update-founditem-approve-url'),
 ]
